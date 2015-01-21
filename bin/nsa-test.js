633f3fb04687eea1e198aaa50c2601e402e43e6e
Michael Kreil <github@michael-kreil.de>
2015-01-21 20:12:23 +0100

    using "real" test instances

--- bin/nsa-test.js
+++ bin/nsa-test.js
@@ -5,18 +5,39 @@ var nsa = require(path.resolve(__dirname, "../nsa.js"));
 
 var hearts = [];
 
-for (var i = 0; i < 23; i++) {
-	(function(i){
-		hearts.push(new nsa({
-			server: "udp4:127.0.0.1:46002",
-			service: "nsatest",
-			node: "node"+i,
-			interval: "10s"
-		}).start(function(){
-			console.log("node "+i+" started");
-		}));
-	})(i);
-};
+var instances = [
+	{service:'lovemeter', node:'hydra.opendatacity.de'},
+	{service:'lobbyradar', node:'nyx.datenfreunde.net'},
+	{service:'lettycalypse', node:'hydra.opendatacity.de'},
+	{service:'blablaπ', node:'nyx.datenfreunde.net'},
+	{service:'datenblumen', node:'nyx.datenfreunde.net'},
+	{service:'renderit', node:'styx.opendatacity.de'},
+	{service:'cccountdown', node:'hydra.opendatacity.de'},
+	{service:'smarties', node:'kiesinger.okfn.de'},
+	{service:'tilethief', node:'kiesinger.okfn.de'},
+	{service:'zugsonar', node:'styx.opendatacity.de'},
+	{service:'lobbymail', node:'nyx.datenfreunde.net'},
+	{service:'flightradar', node:'nyx.datenfreunde.net'},
+	{service:'tilecop', node:'nyx.datenfreunde.net'},
+	{service:'tilethief', node:'nyx.datenfreunde.net'},
+	{service:'tileit', node:'styx.opendatacity.de'},
+	{service:'tilecop', node:'styx.opendatacity.de'},
+	{service:'ships', node:'styx.opendatacity.de'},
+	{service:'tilethief', node:'styx.opendatacity.de'},
+	{service:'tilethief', node:'mcp.netzguerilla.net'},
+	{service:'tilethief', node:'jam.netzguerilla.net'}
+]
+
+instances.forEach(function (instance, index) {
+	hearts.push(new nsa({
+		server: "udp4:127.0.0.1:46002",
+		service: instance.service,
+		node: instance.node,
+		interval: "10s"
+	}).start(function() {
+		console.log("node "+index+" started");
+	}));
+});
 
 process.on("SIGINT", function(){
 	console.log("stopping...")
